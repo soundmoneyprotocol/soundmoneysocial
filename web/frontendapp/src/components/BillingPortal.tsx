da26df1876285aef80e62a4fc15c6f95f5dadc00
soundmoneyprotocol <casmirconsults@gmail.com>
2026-03-13 18:11:01 -0700

    fix: correct Button import syntax in BillingPortal (default export)

--- web/frontendapp/src/components/BillingPortal.tsx
+++ web/frontendapp/src/components/BillingPortal.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useAuth } from '../contexts/AuthContext';
-import { Button } from './Button';
+import Button from './Button';
 import { theme } from '../theme/theme';
 
 export interface BillingPortalProps {
